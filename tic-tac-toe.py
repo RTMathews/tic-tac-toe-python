31d1d2f9f9f22d53bf1163a60ceef1644eb97eba
Ryan Mathews <MathewsThomasRyan@gmail.com>
2025-04-15 01:01:20 -0500

    created place_mark func

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -6,9 +6,9 @@ def show_board(board):
     '''
     #prints tic-tac-toe board
 
-    print(board[7]+'|'+board[8]+'|'+board[9])
-    print(board[4]+'|'+board[5]+'|'+board[6])
-    print(board[1]+'|'+board[2]+'|'+board[3])
+    print(board[7]+'| '+board[8]+' |'+board[9])
+    print(board[4]+'| '+board[5]+' |'+board[6])
+    print(board[1]+'| '+board[2]+' |'+board[3])
 
 
 def player_input():
@@ -28,4 +28,17 @@ def player_input():
     
     return (player1_mark, player2_mark)
 
-player_input()
+
+def place_mark(board,mark,position):
+    '''
+    This function let's the player choose which cell to put their mark in.
+    '''
+    choice = ''
+    accepted_list = [1,2,3,4,5,6,7,8,9]
+
+    while choice not in accepted_list:
+        choice = int(input('Enter the number of the cell you want to place your mark.: '))
+    board[position] = mark
+
+
+game_board = ['','','','','','','','','','']
