b03f2fb579cbb0a31bc31e38efa3cb8868b4f7b9
Ryan Mathews <MathewsThomasRyan@gmail.com>
2025-04-14 20:13:01 -0500

    added player_position func

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -2,10 +2,11 @@ def show_board(board):
     '''
     This function will display and keep the tic tac toe board updated during the game.
     '''
+    #prints tic-tac-toe board
+
     print(board[7]+'|'+board[8]+'|'+board[9])
     print(board[4]+'|'+board[5]+'|'+board[6])
     print(board[1]+'|'+board[2]+'|'+board[3])
-
 
 
 def player_input():
@@ -28,3 +29,17 @@ def player_input():
         player2 = 'X'
 
     return (player1,player2)
+
+
+def player_position(board, mark, position):
+    '''
+    This function will ask the player which tile they want to place their mark in, 1 - 9.
+    '''
+    accepted_values = [1,2,3,4,5,6,7,8,9]
+
+    while position not in accepted_values and position != '':
+        position = input("Choose your next position: (1-9)")
+        if position == '':
+            board[position].replace('',mark)
+
+    return board
